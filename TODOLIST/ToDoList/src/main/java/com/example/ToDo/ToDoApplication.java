fd4e1ea643f5d11a65453dc4e76691cbca99421f
Arathy <arathyp.radhakrishnan@gmail.com>
2024-05-14 22:57:18 +0530

    final commit

---
+++ TODOLIST/ToDoList/src/main/java/com/example/ToDo/ToDoApplication.java
@@ -0,0 +1,15 @@
+package com.example.ToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@SpringBootApplication
+public class ToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplication.class, args);
+	}
+
+}
